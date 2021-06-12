1fa976e84b8d7a46d75e794f73feed81921453fc
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-12 18:14:30 -0300

    Add code that identified number of lines and columns

---
+++ Easy6502/display-drawing.asm
@@ -0,0 +1,45 @@
+LDA #$01
+STA 512  ; primeiro = 0
+
+LDA #$01
+STA 544  ; 32 por linha
+
+LDA #$01
+STA 576  ; 32 por linha
+
+LDA #$01
+STA 608  ; 32 por linha
+
+LDA #$01
+STA 640  ; 32 por linha
+
+LDA #$01
+STA 960  ; 32 por linha
+
+LDA #$01
+STA 1280  ; 32 por linha
+
+LDA #$01
+STA 1440  ; 32 por linha
+
+LDA #$01
+STA 1472  ; 32 por linha
+
+LDA #$01
+STA 1504  ; 32 por linha, 31 por coluna
+
+
+
+LDA #$02
+STA 1534 ; ultimo
+
+
+
+LDA #$03
+STA 842  ;  
+
+LDA #$04
+STA 1444  ;
+
+LDA #$05
+STA 1444  ;
